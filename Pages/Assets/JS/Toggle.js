b2f63075f2e791ef20bc165e793e6fd18b44fc53
GamingLightFam <GamingLightFam@gmail.com>
2024-10-31 18:14:53 +0800

    Add files via upload

---
+++ Pages/Assets/JS/Toggle.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const mobileMenu = document.getElementById("mobile-menu");
+    const navLinks = document.querySelector(".nav-links");
+
+    mobileMenu.addEventListener("click", function () {
+        navLinks.classList.toggle("active"); // Toggle the active class on click
+    });
+});
